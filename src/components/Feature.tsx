690d84298af3329fe3f5fa60080b09fcd9d815ee
KAULBHASKAR <kaultantra@gmail.com>
2026-08-28 10:08:04 +0530

    Update Feature.tsx

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -133,15 +133,7 @@ const Feature: FC = () => {
 
           <BentoTilt className="bento-title_1 md:col-span-1">
             <BentoCard
-             <img
-          src="/img/ABOUT_US.png"
-          alt="yoga background"
-          width={1440}
-          height={580}
-          loading="lazy"
-          decoding="async"
-          className="w-screen h-screen object-cover object-center 2xl:rounded-s-xl"
-        />
+              src="videos/3d.mp4"
               title={
                 <>
                   <b>metaphysical rituals &</b> <b>deployments</b>
