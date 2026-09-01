ed19978a237540f4c107d5e7f6a5751e5f32f599
KAULBHASKAR <kaultantra@gmail.com>
2026-09-01 12:45:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import Layout from './components/Layout';
-import ScrollToTop from './components/ScrollToTop'; // 👈 Move it here
 
 
 // 1. Lazy load all page components
@@ -13,8 +12,7 @@ const Blog = lazy(() => import('./routes/Blog'));
 const BlogPost = lazy(() => import('./routes/BlogPost'));
 const Contact = lazy(() => import('./routes/Contact'));
 const Profile = lazy(() => import('./routes/Profile'));
-
-
+const ScrollToTop = lazy(() => import('./components/ScrollToTop'));
 
 // 2. Helper to wrap elements in Suspense for cleaner code
 const withSuspense = (Component: React.ReactNode) => (
