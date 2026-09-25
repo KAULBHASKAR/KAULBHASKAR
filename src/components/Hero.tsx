3cef7578acb51d7e4dbbabadc370a3a522e2baf2
KAULBHASKAR <kaultantra@gmail.com>
2026-09-25 22:08:12 +0530

    Refactor Hero component for video handling and animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -181,7 +181,7 @@ const Hero: React.FC = () => {
           </a>
         </div>
       </div>
-      <h1 className="special-font hero-heading absolute bottom-5 right-5 ">
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-white ">
         BH<b>as</b>k<b>a</b>r
       </h1>
     </div>
